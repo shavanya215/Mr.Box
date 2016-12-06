75ee8e2d968ba866ef66991fca15a32fffac90f1
shavanya215 <shavanya.215@gmail.com>
2016-12-06 16:42:38 -0500

    scirpts

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+}
