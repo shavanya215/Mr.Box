c79c3e3562ab0520c7a50aa2af49fd9ca370cfcb
shavanya215 <shavanya.215@gmail.com>
2016-12-13 13:00:05 -0500

    Time and Losing

---
+++ Assets/Scripts/Lose Box.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseBox : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Player").SendMessage("Finished");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    public float startTime;
+    private bool finished = false;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (finished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+	
+	}
+
+    public void Finish ()
+    {
+        finished = true;
+        timerText.color = Color.yellow;
+    }
+}
